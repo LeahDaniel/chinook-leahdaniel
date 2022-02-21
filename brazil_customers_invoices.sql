f12ab4b2173f3b10b915e00274cd04dcc900e01d
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-21 10:30:26 -0600

    #3

---
+++ brazil_customers_invoices.sql
@@ -0,0 +1,12 @@
+-- Provide a query showing the Invoices of Customers who are from Brazil. 
+-- The resultant table should include:
+-- Customer's full name
+-- Invoice Id,
+-- Date of the invoice
+-- Billing country
+
+SELECT c.firstName, c.lastName, i.invoiceId, i.invoiceDate, i.billingCountry
+FROM Customer c
+JOIN Invoice i ON i.customerId = c.customerId
+WHERE c.country IS "Brazil"
+
