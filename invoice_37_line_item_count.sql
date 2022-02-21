a144f405b97270d53eee88090223465582086fad
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-21 11:26:58 -0600

    #10

---
+++ invoice_37_line_item_count.sql
@@ -0,0 +1,7 @@
+-- Looking at the InvoiceLine table, provide a query
+-- that COUNTs the number of line items for Invoice ID 37.
+
+SELECT COUNT(*) total, invoiceId
+FROM InvoiceLine
+WHERE invoiceId = 37
+GROUP BY invoiceId
